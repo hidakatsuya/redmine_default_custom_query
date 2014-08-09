0f954aa1713f466b112d1fc3b3596cd6a56f8572
Katsuya Hidaka <hidakatsuya@gmail.com>
2014-08-09 23:33:45 +0900

    Fixed mistake

--- test/factories/project.rb
+++ test/factories/project.rb
@@ -2,11 +2,11 @@ FactoryGirl.define do
   factory :project do
     sequence(:name) {|n| "Project#{n}"}
     sequence(:identifier) {|n| "project-#{n}"}
-  end
 
-  trait :with_default_custom_query do
-    after(:create) do |project|
-      create :default_custom_query_module, project: project
+    trait :with_default_custom_query do
+      after(:create) do |project|
+        create :default_custom_query_module, project: project
+      end
     end
   end
 end
