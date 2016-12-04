ece4462ec42bd3c8d57cfc0af20a1675b7b5fc63
Katsuya HIDAKA <hidakatsuya@gmail.com>
2016-12-04 15:18:01 +0900

    Remove unnecessary code

--- test/factories/issue_query.rb
+++ test/factories/issue_query.rb
@@ -4,30 +4,20 @@ FactoryGirl.define do
 
     user_id { User.current.id }
 
-    if ::Redmine::VERSION.to_s < '2.4'
-      trait :private do
-        is_public false
-      end
+    trait :private do
+      visibility ::Query::VISIBILITY_PRIVATE
+    end
 
-      trait :public do
-        is_public true
-      end
-    else
-      trait :private do
-        visibility ::Query::VISIBILITY_PRIVATE
-      end
+    trait :roles do
+      visibility ::Query::VISIBILITY_ROLES
 
-      trait :roles do
-        visibility ::Query::VISIBILITY_ROLES
-
-        before(:create) do |query|
-          query.roles << create(:role)
-        end
+      before(:create) do |query|
+        query.roles << create(:role)
       end
+    end
 
-      trait :public do
-        visibility ::Query::VISIBILITY_PUBLIC
-      end
+    trait :public do
+      visibility ::Query::VISIBILITY_PUBLIC
     end
   end
 end
