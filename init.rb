be49dcc0b65a937c60d4779362d4e363ccfbae78
Minoru Maeda <hogehogefoozoobar@gmail.com>
2019-10-27 15:02:02 +0900

    Version 1.5.0

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_default_custom_query do
   name 'Redmine Default Custom Query'
   author 'Katsuya Hidaka'
   description 'Redmine plugin for setting default custom query of Issues for each project'
-  version '1.4.0'
+  version '1.5.0'
   requires_redmine '4.0'
   url 'https://github.com/hidakatsuya/redmine_default_custom_query'
   author_url 'https://twitter.com/hidakatsuya'
